4ec313d9785828fa3053c54367e8ba2a887bda06
cogsincogs <jamiewthomas13@gmail.com>
2023-07-04 15:10:21 +0100

    Create quick test run of Express + MongoDB

--- app/server.js
+++ app/server.js
@@ -1,16 +1,38 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const URI = process.env.ATLAS_URI;
 
 // App
 const app = express();
+
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect(URI);    
+}
+
+const Cat = mongoose.model('Cat', { name: String });
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    const kitty = new Cat({ name: "Louis" });
+    kitty.save();
+
+    res.send(kitty);
 });
+
+app.get('/cats', (req, res) => {
+    Cat.find({ name: "Louis" }).then(cats => {
+        res.send(cats);
+    }).catch(err => {
+        res.send("Error: " + err);        
+    })
+})
 
 app.listen(PORT, HOST, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
